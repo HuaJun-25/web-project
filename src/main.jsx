dc52e065b5a64b98b98f856db3039f63e1381144
HuaJun-25 <izuankwii@gmail.com>
2025-08-31 23:54:22 +0800

    home

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import App from './pages/test06.jsx'
+import App from './pages/test07.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ src/pages/test07.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div style={{
+        height:"100vh",
+        width:'100vw',
+        backgroundColor:"#555",
+    }} ></div>
+     <div style={{
+        height:"100vh",
+        width:'100vw',
+        backgroundColor:"#f69898ff",
+    }} ></div>
+    </>
+  )
+}
+
+export default App
