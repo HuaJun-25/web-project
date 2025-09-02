441311ef1cc8ef93d3a2ed788f12f479d3d6c674
HuaJun-25 <izuankwii@gmail.com>
2025-09-02 16:03:42 +0800

    測試sticky

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import App from './pages/test07.jsx'
+import App from './pages/test08.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ src/pages/test08.jsx
@@ -0,0 +1,46 @@
+import '../css/test08.css'
+
+const App = () => {
+  return (
+    <>
+        <div className="inner">
+            <div className="purple"></div>
+            <div className="green"></div>
+            <div className="yellow">
+                <div className='photo'>
+                    <div className="iimg"></div>
+                    <h3>title</h3>
+                </div>
+                <div className='ttxt'>
+                    <div>
+                        <h3>01</h3>
+                        <p>-說明文字</p>
+                    </div>
+                    <div>
+                        <h3>02</h3>
+                        <p>-說明文字</p>
+                    </div>
+                    <div>
+                        <h3>03</h3>
+                        <p>-說明文字</p>
+                    </div>
+                    <div>
+                        <h3>04</h3>
+                        <p>-說明文字</p>
+                    </div>
+                    <div>
+                        <h3>05</h3>
+                        <p>-說明文字</p>
+                    </div>
+
+                </div>
+            </div>
+            <div className="blue"></div>
+            <div className="gray"></div>
+        </div>
+    </>
+
+  )
+}
+
+export default App
